6f045b98a8259235ffdfd5c8bf7f5a19b7a86414
Dvalii1 <33378171+Dvalii1@users.noreply.github.com>
2017-11-21 23:16:25 +0400

    Update skript.js

--- skript.js
+++ skript.js
@@ -34,27 +34,4 @@ var data = {
 }
 
 function checkConnected(locationData, firstId, secondId){
-    //get first location object by ID if exists
-    var firstLocation = locationData.locations.filter(function(location){ return location.Id == firstId });
-    // get connected locations
-    var firstLocationIds = firstLocation.length ? firstLocation[0]['ConnectedLocationIds'] : [];
-    
-    // check one way
-    var checkOne = firstLocationIds.includes(secondId);
-    
-    if(checkOne){
-      return true;
-    }
-    
-    // check two ways
-    var checkTwo = locationData.locations.filter(function(location){
-        // where ConnectedLocationIds includes second location
-        return location.ConnectedLocationIds.includes(secondId);  
-    }).filter(function(location){
-        return firstLocation[0]['ConnectedLocationIds'].includes(location.Id);
-    });
-    
-    return checkTwo.length ? true : false;
 }
-
-console.log(checkConnected(data,3,5));
